c0210ec84e1ef6b8533e068a8dee6edd9726d66f
cys940 <56954032+cys940@users.noreply.github.com>
2022-06-16 15:36:24 +0900

    <commit> by cys 1. viewModel apply 2. dataBinding apply 3. liveData apply

--- app/src/main/java/kr/gmtc/resttest/MainActivity.java
+++ app/src/main/java/kr/gmtc/resttest/MainActivity.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import java.util.List;
 
+import kr.gmtc.resttest.databinding.ActivityMainBinding;
 import kr.gmtc.resttest.model.ht08.cctv.Cctv;
 import kr.gmtc.resttest.model.ht08.cfg.SystemConfig;
 import kr.gmtc.resttest.model.ht08.equip.Device;
@@ -16,78 +20,15 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding = null;
+    private RestViewModel viewModel = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(RestViewModel.class);
 
-        RestClient client = RestClient.getInstance()
-                .setUrl("http://192.168.12.211", 8083)
-                .setAuthId("gmt")
-                .setAuthPw("gmtvision")
-                .setReadTimeout(3)
-                .setWriteTimeout(3)
-                .setRetryConnect(true)
-                .setRetrofit();
-
-        getAllDevices(client);
-    }
-
-    private void getAllDevices(RestClient client){
-        retrofit2.Call<List<Device>> call = client.getService().getAllDevices();
-
-        call.enqueue(new Callback<List<Device>>() {
-            @Override
-            public void onResponse(Call<List<Device>> call, Response<List<Device>> response) {
-                if (response.isSuccessful()) {
-                    for (Device allDevice : response.body()){
-                        Log.d("TAG", allDevice.toString());
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Device>> call, Throwable t) {
-                t.printStackTrace();
-            }
-        });
-    }
-
-    private void getAllCctvs(RestClient client){
-        retrofit2.Call<List<Cctv>> call = client.getService().getAllCctvs();
-
-        call.enqueue(new Callback<List<Cctv>>() {
-            @Override
-            public void onResponse(Call<List<Cctv>> call, Response<List<Cctv>> response) {
-                if (response.isSuccessful()) {
-                    for (Cctv cctv : response.body()){
-                        Log.d("TAG", cctv.toString());
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Cctv>> call, Throwable t) {
-
-            }
-        });
-    }
-
-    private void getSystemConfig(RestClient client){
-        retrofit2.Call<SystemConfig> call = client.getService().getSystemConfig();
-
-        call.enqueue(new Callback<SystemConfig>() {
-            @Override
-            public void onResponse(Call<SystemConfig> call, Response<SystemConfig> response) {
-                if (response.isSuccessful()) {
-                    Log.d("TAG", response.body().toString());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<SystemConfig> call, Throwable t) {
-
-            }
-        });
+        viewModel.getCurrentLog().observe(this, s -> binding.result.setText(s));
     }
 }
---
+++ app/src/main/java/kr/gmtc/resttest/RestViewModel.java
@@ -0,0 +1,131 @@
+package kr.gmtc.resttest;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.gmtc.resttest.model.ht08.cctv.Cctv;
+import kr.gmtc.resttest.model.ht08.cfg.SystemConfig;
+import kr.gmtc.resttest.model.ht08.equip.Device;
+import kr.gmtc.resttest.rest.RestClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RestViewModel extends ViewModel {
+    private MutableLiveData<List<String>> list;
+    public LiveData<List<String>> getAll() {
+        if (list == null) {
+            list = new MutableLiveData<List<String>>();
+            fetch();
+        }
+        return list;
+    }
+
+    private MutableLiveData<String> log;
+    public LiveData<String> getCurrentLog() {
+        if (log == null) {
+            log = new MutableLiveData<String>();
+        }
+        return log;
+    }
+
+    private void fetch(){
+        List<String> samples = new ArrayList<>();
+        samples.add("getAllDevices");
+        samples.add("getAllCctvs");
+        samples.add("getSystemConfig");
+
+        list.setValue(samples);
+    }
+
+    public void getAllDevices(){
+        retrofit2.Call<List<Device>> call = RestClient.getInstance()
+                .setUrl("http://192.168.12.211", 8083)
+                .setAuthId("gmt")
+                .setAuthPw("gmtvision")
+                .setReadTimeout(3)
+                .setWriteTimeout(3)
+                .setRetryConnect(true)
+                .setRetrofit()
+                .getService()
+                .getAllDevices();
+
+        call.enqueue(new Callback<List<Device>>() {
+            @Override
+            public void onResponse(Call<List<Device>> call, Response<List<Device>> response) {
+                if (response.isSuccessful()) {
+                    for (Device allDevice : response.body()){
+                        log.setValue(allDevice.toString());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Device>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+
+    public void getAllCctvs(){
+        retrofit2.Call<List<Cctv>> call = RestClient.getInstance()
+                .setUrl("http://192.168.12.211", 8083)
+                .setAuthId("gmt")
+                .setAuthPw("gmtvision")
+                .setReadTimeout(3)
+                .setWriteTimeout(3)
+                .setRetryConnect(true)
+                .setRetrofit()
+                .getService()
+                .getAllCctvs();
+
+        call.enqueue(new Callback<List<Cctv>>() {
+            @Override
+            public void onResponse(Call<List<Cctv>> call, Response<List<Cctv>> response) {
+                if (response.isSuccessful()) {
+                    for (Cctv cctv : response.body()){
+                        log.setValue(cctv.toString());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Cctv>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public void getSystemConfig(){
+        retrofit2.Call<SystemConfig> call = RestClient.getInstance()
+                .setUrl("http://192.168.12.211", 8083)
+                .setAuthId("gmt")
+                .setAuthPw("gmtvision")
+                .setReadTimeout(3)
+                .setWriteTimeout(3)
+                .setRetryConnect(true)
+                .setRetrofit()
+                .getService()
+                .getSystemConfig();
+
+        call.enqueue(new Callback<SystemConfig>() {
+            @Override
+            public void onResponse(Call<SystemConfig> call, Response<SystemConfig> response) {
+                if (response.isSuccessful()) {
+                    log.setValue(response.body().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SystemConfig> call, Throwable t) {
+
+            }
+        });
+    }
+}
